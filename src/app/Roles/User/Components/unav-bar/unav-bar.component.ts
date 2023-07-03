ec32b54fcf9814e32db1f5f83be4dfee77fc9150
SRINI-KS <sriniks887@gmail.com>
2023-07-03 22:02:20 +0530

    tailwind

--- src/app/Roles/User/Components/unav-bar/unav-bar.component.ts
+++ src/app/Roles/User/Components/unav-bar/unav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { AuthServiceService } from 'src/app/Services/auth-service.service';
 
 @Component({
@@ -6,9 +6,21 @@ import { AuthServiceService } from 'src/app/Services/auth-service.service';
   templateUrl: './unav-bar.component.html',
   styleUrls: ['./unav-bar.component.css']
 })
-export class UnavBarComponent {
+export class UnavBarComponent implements AfterViewInit{
+   @ViewChild ('menu_button')  button!:ElementRef;
+   @ViewChild ('menu')  menu!:ElementRef;
+   @ViewChildren(UnavBarComponent) el?:QueryList<UnavBarComponent>;
+
+  constructor(private authService:AuthServiceService){
   
-  constructor(private authService:AuthServiceService){}
+  }
+  ngAfterViewInit(): void {
+    this.button.nativeElement.addEventListener('click', () => {
+      console.log("dskfjalskfj")
+        this.menu.nativeElement.classList.toggle('hidden');
+       });
+       this.el?.forEach((el)=>console.log("menu",el))
+  }
 
   // username
   userName=this.authService.getUserName()
@@ -17,4 +29,9 @@ export class UnavBarComponent {
   }
 
 
+  
+  
+ 
+
+  
 }
